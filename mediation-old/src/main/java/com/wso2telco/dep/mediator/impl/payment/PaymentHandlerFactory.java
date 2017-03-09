f802e5fbe70d6589aa169017b513b640f8b7d152
Shashika Wijayasekera <shashika@wso2telco.com>
2017-03-09 18:07:34 +0530

    Fixed EXTGW-20 and EXTGW-35

--- mediation-old/src/main/java/com/wso2telco/dep/mediator/impl/payment/PaymentHandlerFactory.java
+++ mediation-old/src/main/java/com/wso2telco/dep/mediator/impl/payment/PaymentHandlerFactory.java
@@ -142,18 +142,22 @@ public class PaymentHandlerFactory {
 					JSONObject objPaymentAmount = (JSONObject) objAmountTransaction.get("paymentAmount");
 					JSONObject objchargingInformation = (JSONObject) objPaymentAmount.get("chargingInformation");
 
-					if(!objchargingInformation.get("currency").equals("") && objchargingInformation.get("amount").equals("")){
+					if(!objchargingInformation.has("currency") && objchargingInformation.has("amount")){
+						
+						log.debug("createPaymentHandler -> parameter not found.");
+						throw new CustomException("SVC0002", "",
+								new String[] { "Missing mandatory parameter: currency" });
+					}
+					else if (!objchargingInformation.has("amount") && objchargingInformation.has("currency")){
+						
 						log.debug("createPaymentHandler -> parameter not found.");
 						throw new CustomException("SVC0002", "",new String[] { "Missing mandatory parameter: amount" });
-					}
-					else if (!objchargingInformation.get("amount").equals("") && objchargingInformation.get("currency").equals("")){
-						log.debug("createPaymentHandler -> parameter not found.");
-						throw new CustomException("SVC0002", "",new String[] { "Missing mandatory parameter: currency" });
-					}else if(objchargingInformation.get("amount").equals("") && objchargingInformation.get("currency").equals("")){
+					}else if(!objchargingInformation.has("amount") && !objchargingInformation.has("currency")){
 
-						if(objchargingInformation.get("code").equals("")){
+						if(!objchargingInformation.has("code")){
+							
 							log.debug("createPaymentHandler -> parameter not found.");
-							throw new CustomException("SVC0007", "",new String[] { "" });
+							throw new CustomException("SVC0002", "",new String[] { "Missing mandatory parameter: amount or code" });
 						}
 					}
 				} catch (CustomException e) {
