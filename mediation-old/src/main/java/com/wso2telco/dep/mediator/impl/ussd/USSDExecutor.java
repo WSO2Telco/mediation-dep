91ea90831f9d2cec2b9dbd36b94629515a9524ee
Priyanka <priyankahdp@gmail.com>
2017-05-16 17:40:39 +0530

    Ussd data-Publishing

--- mediation-old/src/main/java/com/wso2telco/dep/mediator/impl/ussd/USSDExecutor.java
+++ mediation-old/src/main/java/com/wso2telco/dep/mediator/impl/ussd/USSDExecutor.java
@@ -17,12 +17,22 @@
  */
 package com.wso2telco.dep.mediator.impl.ussd;
 
+import com.wso2telco.dep.mediator.MSISDNConstants;
+import com.wso2telco.dep.mediator.OperatorEndpoint;
 import com.wso2telco.dep.mediator.RequestExecutor;
+import com.wso2telco.dep.mediator.internal.ResourceURLUtil;
+import com.wso2telco.dep.mediator.internal.Type;
+import com.wso2telco.dep.mediator.internal.UID;
+import com.wso2telco.dep.mediator.mediationrule.OriginatingCountryCalculatorIDD;
+import com.wso2telco.dep.mediator.util.HandlerUtils;
 import com.wso2telco.dep.oneapivalidation.exceptions.CustomException;
+import com.wso2telco.dep.subscriptionvalidator.util.ValidatorUtils;
+
 import org.apache.axis2.AxisFault;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.synapse.MessageContext;
+import org.apache.synapse.core.axis2.Axis2MessageContext;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -35,7 +45,11 @@ public class USSDExecutor extends RequestExecutor {
 
     /** The log. */
     private Log log = LogFactory.getLog(USSDExecutor.class);
-
+    private OriginatingCountryCalculatorIDD occi;
+    
+    public USSDExecutor() {
+        occi = new OriginatingCountryCalculatorIDD();
+    }
     /** The handler. */
     private USSDHandler handler;
 
@@ -44,10 +58,32 @@ public class USSDExecutor extends RequestExecutor {
      */
     @Override
     public boolean execute(MessageContext context) throws CustomException, AxisFault, Exception {
-        try {
+        try {    	
+        	//String requestid = UID.getUniqueID(Type.SEND_USSD.getCode(), context, getApplicationid());
+        	JSONObject jsonBody = getJsonBody();
+    		String address = jsonBody.getJSONObject("outboundUSSDMessageRequest").getString("address");
+    		String msisdn = address.substring(5);
+    		
+        	context.setProperty(MSISDNConstants.MSISDN, address);
+        	context.setProperty(MSISDNConstants.USER_MSISDN, msisdn);
+    		
+    		OperatorEndpoint endpoint = null;
+            if (ValidatorUtils.getValidatorForSubscriptionFromMessageContext(context).validate(context)) {
+                endpoint = occi.getAPIEndpointsByMSISDN(address.replace("tel:", ""), "ussd", getSubResourcePath(), true,getValidoperators());
+            }
+            String sending_add = endpoint.getEndpointref().getAddress();
+    		sending_add += getSubResourcePath();
+            
+    		HandlerUtils.setHandlerProperty(context,this.getClass().getSimpleName());
+    		HandlerUtils.setEndpointProperty(context,sending_add);
+    		HandlerUtils.setAuthorizationHeader(context,this,endpoint);
+    		
+            context.setProperty("operator", endpoint.getOperator());
+            context.setProperty("OPERATOR_NAME", endpoint.getOperator());
+            context.setProperty("OPERATOR_ID", endpoint.getOperatorId());
+    			
             USSDHandler handler = getUSSDHandler(getSubResourcePath());
-            return handler.handle(context);
-
+	        return handler.handle(context);
         } catch (JSONException e) {
             log.error(e.getMessage());
             throw new CustomException("SVC0001", "", new String[]{"Request is missing required URI components"});
