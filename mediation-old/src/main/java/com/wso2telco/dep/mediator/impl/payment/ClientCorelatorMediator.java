5250d41d8216b7ea0f25b988c8b432b1b7d98b33
shalith <shalith@wso2telco.com>
2019-04-08 17:47:24 +0530

    EXTGW-153 adding class mediators

---
+++ mediation-old/src/main/java/com/wso2telco/dep/mediator/impl/payment/ClientCorelatorMediator.java
@@ -0,0 +1,43 @@
+package com.wso2telco.dep.mediator.impl.payment;
+
+import org.apache.synapse.MessageContext; 
+import org.apache.synapse.core.axis2.Axis2MessageContext;
+import org.apache.synapse.mediators.AbstractMediator;
+import org.apache.synapse.commons.json.JsonUtil;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+public class ClientCorelatorMediator extends AbstractMediator { 
+
+	public boolean mediate(MessageContext context) { 
+		
+		try{
+
+				    if(context.getProperty("clientCorrelator") == null){
+				    	return true;
+					}
+
+			String jsonPayloadToString = JsonUtil.jsonPayloadToString(((Axis2MessageContext) context)
+					.getAxis2MessageContext());
+
+			JSONObject jsonBody = new JSONObject(jsonPayloadToString);
+			String clientCorrelatorNew = context.getProperty("clientCorrelator").toString() ;
+			log.info("Client Correlator New : "+clientCorrelatorNew);
+			jsonBody.put("amountTransaction.clientCorrelator", clientCorrelatorNew);
+			((JSONObject)jsonBody.get("amountTransaction")).put("clientCorrelator",clientCorrelatorNew);
+
+				    String transformedJson = jsonBody.toString();
+			log.info("New transformed JSON : " + transformedJson);
+			JsonUtil.newJsonPayload(
+				    ((Axis2MessageContext) context).getAxis2MessageContext(),
+				    transformedJson, true, true);
+		    
+		}catch(JSONException e){
+			log.error("Error occured while processing JSON : ",e);
+			
+		}
+		
+		return true;
+	}
+}
